d403dfa169f85e213a2d5a2e590429534256f3c3
prabhatvrm <88664925+prabhatvrm@users.noreply.github.com>
2024-02-13 18:36:40 +0530

    Delete gh_activity directory

--- gh_activity/write.py
+++
@@ -1,7 +0,0 @@
-def gh_write(df,tgt_path):
-    df.coalesce(16). \
-        write. \
-        partitionBy('year', 'month', 'day'). \
-        mode('append'). \
-        format("json"). \
-        save(tgt_path)
